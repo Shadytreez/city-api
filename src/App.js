f8319b9a27127eb8a1d4ed6ce8abc442a0ec9601
Truman <trumanngny@yahoo.com>
2021-06-10 23:41:02 -0400

    create app.js and city.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import ZipCode from "./zipcode"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      codes:[],
+    };
+  }
+
+  getCities()
+  { const city = document.getElementById("city").value;
+    const url = "https://ctp-zip-api.herokuapp.com/city/"+city;
+    fetch(url).
+    then(response => response.json()).
+    then(data => {
+      console.log(data);
+      //this.setState({codes: data});
+    }).
+    catch(error => console.log("error:"));
+    
+  }
+
+  
+
+  render(){
+
+    
+
+    return (
+    <div>
+      <h1>City Search</h1>
+      <form id ="form">
+         <p>City </p> <input id="city" placeholder="newyork" type="text"/>
+         <button type ="button" onClick={this.getCities}> Submit</button>
+       </form>
+       {this.getCityBar}
+    </div>)
+  }
 }
-
 export default App;
---
+++ src/zipcode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ZipCode(props){
+    return(
+        <div>
+            <h1>{props.city.LocationText}</h1>
+        </div>
+    )
+}
+
+export default ZipCode;
